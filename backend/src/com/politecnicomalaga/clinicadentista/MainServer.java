530b9a58a35c2e45c8fbbc997c26c9a843ee6ea7
GonzaloR44 <gruimor624@g.educaand.es>
2023-05-15 12:55:55 +0200

    Commit de preparación

---
+++ backend/src/com/politecnicomalaga/clinicadentista/MainServer.java
@@ -0,0 +1,9 @@
+package com.politecnicomalaga.clinicadentista;
+
+public class MainServer {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- backend/src/com/politecnicomalaga/clinicadentistaTest/ServerTest.java
+++ backend/src/com/politecnicomalaga/clinicadentistaTest/ServerTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.politecnicomalaga.clinicadentista.Tratamiento;
-import com.politecnicomalaga.clinicadentista.Paciente;
 
 class ServerTest {
 	
